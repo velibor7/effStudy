b22e0574c960fb5d7c3e1777e5b6cae577baf014
Velibor <veliborvasiljevic7@gmail.com>
2020-04-27 09:08:13 +0200

    everything is in pomodoro, messy

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pomodoro',
-  templateUrl: './pomodoro.component.html',
-  styleUrls: ['./pomodoro.component.sass']
+  selector: "app-pomodoro",
+  templateUrl: "./pomodoro.component.html",
+  styleUrls: ["./pomodoro.component.sass"],
 })
 export class PomodoroComponent implements OnInit {
+  noOfCycles: number = 0;
+  workMinutes: number = 0;
+  pauseMinutes: number = 0;
+  totalHours: number = 0;
+  totalMinutes: number = 0;
 
-  constructor() { }
+  newTaskName: string;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  set25() {
+    // set timer values for 25 - 5
   }
 
+  set50() {
+    // set timer values for 50 - 10
+  }
+
+  onStart() {
+    // start the timer
+  }
+
+  onPause() {
+    // pause the timer
+  }
+
+  //! === CHECKLIST ===
+
+  onEditChecklistElem() {}
+
+  onDeleteChecklistElem() {}
+
+  onAddTask() {}
 }
